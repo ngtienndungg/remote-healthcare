4838be06e1ffd3da0503a8c15079156fcb0f0a05
Dung Tien <dungnt.works@gmail.com>
2024-05-10 22:01:33 +0700

    initial commit

---
+++ src/main/java/com/example/remote_healthcare/RemoteHealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.example.remote_healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteHealthcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteHealthcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/remote_healthcare/RemoteHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remote_healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteHealthcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
